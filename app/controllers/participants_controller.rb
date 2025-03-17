035941a862bd4ee2496e356237bc3f2db6283350
BrunaBallerini <bruna@webgoal.com.br>
2025-03-17 14:07:14 -0300

    Modificando a participants controller para salvar o array enviado pelo front

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,39 +1,19 @@
 # frozen_string_literal: true
 
 class ParticipantsController < ApplicationController # rubocop:disable Style/Documentation
-  def create
-    participant = Participant.new(
-      name: params[:name],
-      email: params[:email],
-      draw_id: params[:draw_id]
-    )
-    return render json: participant, status: :created if participant.save
-
-    render json: participant.errors, status: :unprocessable_entity
-  end
-
-  def update
-    participant = Participant.find(params[:id])
-    if participant.update(participant_params)
-      render json: participant, status: :ok
-    else
-      render json: { errors: participant.errors.full_messages }, status: :unprocessable_entity
+  def create # rubocop:disable Metrics/MethodLength
+    participants = params[:participants].map do |email|
+      Participant.new(
+        name: '',
+        email:,
+        draw_id: params[:draw_id]
+      )
     end
-  end
 
-  def destroy
-    participant = Participant.find(params[:id])
-    participant.destroy
-    render json: { message: 'Participant deleted successfully' }, status: :ok
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Participant not found' }, status: :not_found
-  rescue StandardError => e
-    render json: { error: "Failed to delete participant: #{e.message}" }, status: :unprocessable_entity
-  end
-
-  private
-
-  def participant_params
-    params.require(:participant).permit(:email)
+    if participants.all?(&:save)
+      render json: participants, status: :created
+    else
+      render json: { errors: participants.map(&:errors) }, status: :unprocessable_entity
+    end
   end
 end
